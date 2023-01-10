9ab45de67bc3fed808b8f934beab95a18f9ca147
krazyt1me <joao.barbosa.albuquerque@itec.ufpa.br>
2023-01-10 08:42:23 -0300

    First exercise of an open file C problem

--- makefile.c
+++ makefile.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
 int main(){
+int a;
+int s[10];
+for (a=0;a<10;a++){
+s[a]=a;
 
-printf("hey i just hate myself");
-return 0;}
+}
+FILE *arquivo =fopen("oi.txt", "wb+");
+fwrite(&s[0],sizeof(int),1,arquivo);
+fclose(arquivo);
+
+
+return 0;
+
+}
+
